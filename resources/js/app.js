8a5466d59c2c0462dc8c22dfc382d25368574bd5
Gen-vCoders <154441671+Gen-vCoders@users.noreply.github.com>
2024-05-12 14:16:04 +0530

    added boot strap and done minor chanes  in css

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,6 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from '../js/layout/App.vue';
 import router from './router/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App).use(router).mount("#app");
